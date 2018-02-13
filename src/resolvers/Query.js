3b403d2ad11ebe1eda011f629622becb62d14ea7
Alain Armand <aarmand.inbox@gmail.com>
2018-02-13 15:03:50 +0100

    added companies and compay queries to schema and resolvers

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -13,7 +13,39 @@ function feed(parent, args, context, info) {
 
   return context.db.query.companies({ first, skip, where }, info);
 }
+function companies(parent, args, context, info) {
+  const { filter, first, skip } = args; // destructure input arguments
+  const where = filter
+    ? {
+        OR: [
+          { url_contains: filter },
+          { description_contains: filter },
+          { name_contains: filter },
+          { id: filter }
+        ]
+      }
+    : {};
+
+  return context.db.query.companies({ first, skip, where }, info);
+}
+function company(parent, args, context, info) {
+  const { filter, first, skip } = args; // destructure input arguments
+  const where = filter
+    ? {
+        OR: [
+          { url_contains: filter },
+          { description_contains: filter },
+          { name_contains: filter },
+          { id: filter }
+        ]
+      }
+    : {};
+
+  return context.db.query.company({ first, skip, where }, info);
+}
 
 module.exports = {
-  feed
+  feed,
+  companies,
+  company
 };
